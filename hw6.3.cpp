2968b3d6a1dfa4dd1c8a3f228109e79cf7a3c811
yanjiel <yanjieliu829@gmail.com>
2023-02-12 09:29:31 -0600

    first commit

--- hw6.3.cpp
+++ hw6.3.cpp
@@ -4,26 +4,51 @@
 #include<iostream>
 #include<format>
 #include<vector>
+#include<initializer_list>
 
 
 using namespace std;
+double getMedianSingle(vector<double> grades) { 
+	double result;
 
-class Grades {
-public:
-	string class_name;
+	return result; 
+}
+
+double getMedianDouble(vector<double> grades) {}
+
+double getMeanAll(vector<double> grades) {}
+
+double getMeanNoMinMax(vector<double> grades) {}
+
+
+struct Course {
+	string name;
 	string instructor;
-	Grades() {}
-	double getMedianSingle(vector<double> grades);
-	double getMedianDouble(vector<double> grades);
+	vector<string> hw;
+	vector<double> grades;
+	double getAverage();
+};
 
+class Student {
+public:
+	Student() {}
+	void setName(string name) { name_ = name; }
+	void setCourses(initializer_list<string> courses) { 
+		std::copy(courses.begin(), courses.end(), back_inserter(courses_));
+	}
+	double getGPA(vector<double> grades);
 private:
-	double grades;
-	int student_count;
-
+	double gpa_;
+	string name_;
+	vector<Course> courses_;
 };
 
 
 int main() {
+	Student alex = Student();
+	alex.setName("Alex");
+	alex.setCourses({ "Advanced C++","Python","Potography" });
+
 
 	return 0;
 }
